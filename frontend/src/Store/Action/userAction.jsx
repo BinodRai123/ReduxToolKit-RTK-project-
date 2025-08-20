4ef4091c146fc0b9738ca1ee72ff115e0d1f589a
binod <br163406@gmail.com>
2025-08-19 23:36:50 -1200

    update user profile data emaill, password

--- frontend/src/Store/Action/userAction.jsx
+++ frontend/src/Store/Action/userAction.jsx
@@ -1,20 +1,20 @@
 import axios from "../../utils/axios";
 import { toast } from "react-toastify";
-import { loadUser } from "../reducers/userSlice"
+import { loadUser, removeUser } from "../reducers/userSlice"
 
 export const asyncCurrentUser = () => async (dispatch, getState) => {
   try {
     const user = JSON.parse(localStorage.getItem("user"));
     if(user) dispatch(loadUser(user));
-    else console.log("user not logged in");
   } catch (error) {
     toast.error(error);
   }
 };
 
-export const asyncLogoutUser = (user) => async (dispatch, getState) => {
+export const asyncLogoutUser = () => async (dispatch) => {
   try {
     localStorage.setItem("user", null);
+    dispatch(removeUser());
   } catch (error) {
     toast.error(error);
   }
@@ -25,7 +25,6 @@ export const asyncLoginUser = (user) => async (dispatch, getState) => {
     const { data } = await axios.get(
       `/users?email=${user.email}&password=${user.password}`
     );
-    console.log(data)
     localStorage.setItem("user",JSON.stringify(data[0]));
   } catch (error) {
     toast.error(error);
--- frontend/src/pages/setting.jsx
+++ frontend/src/pages/setting.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { NavLink, useNavigate } from "react-router-dom";
 
 const Setting = () => {
-  return (
-    <div>Setting</div>
-  )
-}
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+  } = useForm({
+    // defaultValues: {
 
-export default Setting
+    // }
+  });
+
+  const handleFormData = (user) => {
+    user.id = nanoid();
+    user.isAdmin = false;
+    dispatch(asyncRegisterUser(user));
+    navigate("/login");
+  };
+
+  return (
+    <form
+      className="flex flex-col gap-8 mt-16 items-center"
+      onSubmit={handleSubmit(handleFormData)}
+    >
+      <input
+        {...register("name")}
+        className="text-xl font-thin login-form"
+        type="text"
+        placeholder="Enter your Name"
+        required
+      />
+      <input
+        {...register("email")}
+        className="text-xl font-thin login-form"
+        type="email"
+        placeholder="abc@gmail.com"
+        required
+      />
+
+      <input
+        {...register("password")}
+        className="text-xl font-thin login-form"
+        type="password"
+        placeholder="********"
+        required
+      />
+
+      <input className="button" type="submit" value={"Update"} />
+
+        <button className="button" type="button">Log Out</button>
+    </form>
+  );
+};
+
+export default Setting;
