23559f564fa9606c75816fd0278c76382a6842c4
binod rai <br163406@gmail.com>
2025-08-21 11:30:21 +0545

    simple example

--- frontend/src/pages/pageNotFound.jsx
+++ frontend/src/pages/pageNotFound.jsx
@@ -1,6 +1,6 @@
 const PageNotFound = () => {
   return (
-    <div className="h-[80%] text-5xl text-gray-200 flex justify-center items-center">PageNotFound!!</div>
+    <div className="h-[80%] text-5xl text-gray-400 flex justify-center items-center">PageNotFound!!</div>
   )
 }
 
